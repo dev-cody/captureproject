fc4c2cc852cddfb98c93d72e19ba15f6ef42b027
Cody <codykoscielski@gmail.com>
2021-03-05 22:30:16 -0500

    Added an faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react';
 //Page Components
 import AboutSection from '../components/AboutSection';
 import ServcesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection />
             <ServcesSection />
+            <FaqSection />
         </>
     )
 }

4305f5d1f25165c83b04b39bf58a3dd808a99860
Cody <codykoscielski@gmail.com>
2021-03-15 20:22:02 -0400

    Learning about styled components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 //Import Pages
 import AboutUs from './pages/AboutUs';
 
+//import global style
+import GlobalStyle from './components/GlobalStyle';
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
